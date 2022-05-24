92145f6246767d38420dff9708fd002f254f6774
DanielSpindler83 <30247817+DanielSpindler83@users.noreply.github.com>
2022-05-24 11:40:54 +1000

    Apply code style changes already done elsewhere.

--- tests/Pingalot.Core.Tests/PingSessionTests.cs
+++ tests/Pingalot.Core.Tests/PingSessionTests.cs
@@ -16,7 +16,7 @@ namespace Pingalot.Core.Tests
 
 			var pingRequests = new List<PingRequest>();
 
-			var startTime = DateTime.Now;
+			var startTime = new DateTime(2022, 5, 19, 14, 30, 0);
 			TimeSpan duration = new System.TimeSpan(0, 0, 0, 30);
 			var endTime = startTime.Add(duration);
 
@@ -40,8 +40,6 @@ namespace Pingalot.Core.Tests
 			Assert.AreEqual(testPingSession.AverageRoundtrip, 0, "If PacketsReceived is 0, when we attempt to calculate AverageRoundTrip we may trigger a divide by zero exception.");
 		}
 
-
-
 		[TestMethod]
 		public void ConfirmPingSessionStatistics()
 		{
@@ -51,7 +49,7 @@ namespace Pingalot.Core.Tests
 
 			var pingRequests = new List<PingRequest>();
 
-			var startTime = DateTime.Now;
+			var startTime = new DateTime(2022, 5, 19, 14, 30, 0);
 			TimeSpan duration = new System.TimeSpan(0, 0, 0, 30);
 			var endTime = startTime.Add(duration);
 
@@ -60,7 +58,7 @@ namespace Pingalot.Core.Tests
 			{
 				var pingRequest = new PingRequest
 				{
-					Address = new IPAddress(16843009),
+					Address = IPAddress.Loopback,
 					Status = IPStatus.Success,
 					RoundtripTime = 10,
 					TimeToLive = 58,
@@ -77,7 +75,7 @@ namespace Pingalot.Core.Tests
 			{
 				var pingRequest = new PingRequest
 				{
-					Address = new IPAddress(16843009),
+					Address = IPAddress.Loopback,
 					Status = IPStatus.Success,
 					RoundtripTime = 5,
 					TimeToLive = 58,
@@ -94,7 +92,7 @@ namespace Pingalot.Core.Tests
 			{
 				var pingRequest = new PingRequest
 				{
-					Address = new IPAddress(16843009),
+					Address = IPAddress.Loopback,
 					Status = IPStatus.TimedOut,
 					RoundtripTime = 0,
 					TimeToLive = 0,
@@ -109,13 +107,13 @@ namespace Pingalot.Core.Tests
 			var testPingSession = new PingSession(startTime, endTime, duration, pingRequests);
 
 			// assert that stats add up properly - calc them manually and not via debug
-			Assert.IsTrue(testPingSession.PacketsReceived == 9, "PacketsReceived should be 9.");
-			Assert.IsTrue(testPingSession.PacketsSent == 11, "PacketsSent should be 11.");
-			Assert.IsTrue(testPingSession.PacketsLost == 2, "PacketsLost should be 2.");
-			Assert.IsTrue(testPingSession.PacketsLostPercentage == 18.18, "PacketsLostPercentage should be 18.18%");
-			Assert.IsTrue(testPingSession.MinimumRoundtrip == 5, "MinimumRoundtrip should be 5");
-			Assert.IsTrue(testPingSession.MaximumRoundtrip == 10, "MinimumRoundtrip should be 10");
-			Assert.IsTrue(testPingSession.AverageRoundtrip == 8.33, "AverageRoundtrip should equal 75 divided by 9 = 8.33 rounded to two decimal places.");
+			Assert.AreEqual(testPingSession.PacketsReceived, 9, "PacketsReceived should be 9.");
+			Assert.AreEqual(testPingSession.PacketsSent, 11, "PacketsSent should be 11.");
+			Assert.AreEqual(testPingSession.PacketsLost, 2, "PacketsLost should be 2.");
+			Assert.AreEqual(testPingSession.PacketsLostPercentage, 18.18, "PacketsLostPercentage should be 18.18%");
+			Assert.AreEqual(testPingSession.MinimumRoundtrip, 5, "MinimumRoundtrip should be 5");
+			Assert.AreEqual(testPingSession.MaximumRoundtrip, 10, "MinimumRoundtrip should be 10");
+			Assert.AreEqual(testPingSession.AverageRoundtrip, 8.33, "AverageRoundtrip should equal 75 divided by 9 = 8.33 rounded to two decimal places.");
 		}
 	}
 }
